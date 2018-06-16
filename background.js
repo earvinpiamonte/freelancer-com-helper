0397c3119653293b37ff5ae29b35293da6cb4142
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2018-06-16 19:31:05 +0800

    Updated PageStateMatcher pageUrl

--- background.js
+++ background.js
@@ -1,16 +1,1 @@
-
-
-chrome.runtime.onInstalled.addListener(function() {
-
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-		chrome.declarativeContent.onPageChanged.addRules([{
-			conditions: [
-				new chrome.declarativeContent.PageStateMatcher({
-					pageUrl: { urlContains: 'freelancer.com' },
-				})
-			],
-			actions: [new chrome.declarativeContent.ShowPageAction()]
-		}]);
-	});
-
-});
+chrome.runtime.onInstalled.addListener(function(){chrome.declarativeContent.onPageChanged.removeRules(void 0,function(){chrome.declarativeContent.onPageChanged.addRules([{conditions:[new chrome.declarativeContent.PageStateMatcher({pageUrl:{hostEquals:"www.freelancer.com",schemes:["https"]}})],actions:[new chrome.declarativeContent.ShowPageAction]}])})});
